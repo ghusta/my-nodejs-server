0aca60338b9b68e39ce0ba4fe4e96278f0c5244f
Guillaume HUSTA <guillaume.husta@gmail.com>
2024-08-27 17:33:07 +0200

    chore: Enable use of environment variables
    See: https://nodejs.org/en/learn/command-line/how-to-read-environment-variables-from-nodejs

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = process.env.APP_HOSTNAME || '127.0.0.1';
 // const hostname = 'localhost';
-const port = 3000;
+const port = process.env.APP_PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello, World!\n')
-  res.write('\n')
-  res.end(`Page running on Node.js ${process.version}`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello, World!\n')
+    res.write('\n')
+    res.end(`Page running on Node.js ${process.version}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Node.js version: ${process.version}`);
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen({
+    port: port,
+    hostname: hostname
+}, () => {
+    console.log(`Node.js version: ${process.version}`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
